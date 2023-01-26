07d34f2fc83e11027b779e10f9a510dc32a4b511
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2023-01-26 10:20:11 +0100

    Add Integration Testing Task1

---
+++ Integration_Testing/integration-task/cypress/e2e/spec.cy.js
@@ -0,0 +1,14 @@
+describe('My First Test', () => {
+  it('clicks the link "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email').type('fake@email.com')
+
+    
+    cy.get('.action-email').should('have.value', 'fake@email.com')
+  })
+})
